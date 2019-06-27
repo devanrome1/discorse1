e2c601f6902d30a62b7ec4e919da563227a22929
Dev <skater.rome@gmail.com>
2019-06-27 10:23:34 -0400

    1

---
+++ discorse_p1/resources/js/script.js
@@ -0,0 +1,27 @@
+// // $(document).ready(function () {
+// //
+// //     $('.js--section-features').waypoint(function(direction) {
+// //         if(direction === "down") {
+// //             $('nav').addClass('sticky')
+// //         } else {
+// //             $('nav').removeClass('sticky');
+// //         }
+// //     });
+// //
+// // });
+//
+// $(document).ready(function() {
+//
+//
+//     /* For the sticky navigation */
+//     $('.js--section-features').waypoint(function (direction) {
+//         if (direction == "down") {
+//             $('nav').addClass('sticky');
+//         } else {
+//             $('nav').removeClass('sticky');
+//         }
+//     }, {
+//         offset: '60px;'
+//     });
+//
+// });
